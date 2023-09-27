9cb5094126f8fcbf7c67c73f6c0294fd6e184082
AlexAguero <alexmanuelaguerovargas@gmail.com>
2023-09-27 03:51:56 -0600

    avance interface

--- Libraries/transacciones.sv
+++ Libraries/transacciones.sv
@@ -2,8 +2,8 @@
 /////////////Posibles transacciones de la FIFO////////////////
 //////////////////////////////////////////////////////////////
 typedef enum {push, pop,reset} tipo_trans; 
-
-//////////// Transacciones del bus handler (Transacciones que entran y salen de las FIFO)/////////
+///////////////////////////////////////////////////////////////////////////
+//////////////// Transacciones del bus handler/////////////////////////////////////// (Transacciones que entran y salen de las FIFO)////////////////////////////////////////////////////////////////////////////////////////////////
 class trans_bushandler #(parameter pkg_size  = 16,parameter drvrs = 4,parameter broadcast={8{1'b1}});
     rand int retardo; // tiempo de retardo en ciclos de reloj que se debe esperar antes de ejecutar la transacción
   	rand bit[pkg_size-9:0] dato; // este es el dato de la transacción
@@ -66,7 +66,8 @@ module testbench;
 
   // Instancia de la clase trans_bushandler
   trans_bushandler#(pkg_size, drvrs, broadcast) transaction;
-trans_bushandler#(pkg_size, drvrs, broadcast) transaction2;      
+trans_bushandler#(pkg_size, drvrs, broadcast) transaction2;
+trans_bushandler#(pkg_size, drvrs, broadcast) transaction3;
   initial begin
     // Inicializar la semilla del generador de números aleatorios
  
@@ -81,18 +82,28 @@ trans_bushandler#(pkg_size, drvrs, broadcast) transaction2;
     transaction2 = new(
         30, //valor para dato
         5, //valor para retardo
-        $time, // tiepo actual
+        $time, // tiempo actual
         push,//tipo diferente de transacciones
         10, //valor maximo retardo
-        4,//valor dispositivo_tx
-        4,//valor dispositivo_rx
+        2,//valor dispositivo_tx, si pongo valor mayor o igual a 4, el assert se dispara, pero como quiero probar la funcion clean, voy a setear un valor menor a 4 
+        2,//valor dispositivo_rx
         0//No aplico reset
     );
+    transaction3 = new(
+       $urandom_range(1,255), //valor random  para dato
+         $urandom_range(1,75), //valor random para retardo
+         $time, // tiempo actual
+          push,//tipo diferente de transacciones
+         50, //valor maximo retardo
+          $urandom_range(1,3),//valor random dispositivo_tx
+          $urandom_range(1,3),//valor random  dispositivo_rx
+          1// aplico reset
+      );
 
     // Imprimir información sobre la instancia
     transaction.print("Transacción 1");
     transaction2.print("Transaccion 2");
-
+    transaction3.print("Transaccion 3");
    
     // Ejecutar la simulación
     // ...
@@ -100,10 +111,15 @@ trans_bushandler#(pkg_size, drvrs, broadcast) transaction2;
     // Verificar las restricciones
     assert(transaction.inside_driver_range()) else $display("Error: dispositivo_rx fuera de rango");
     assert(transaction2.inside_driver_range()) else $display("Error: dispositivo_rx fuera de rango");
+   assert(transaction3.inside_driver_range()) else $display("Error: disposi    tivo_rx fuera de rango");
+    //reestablecer valores a algo conocido
+     transaction.clean();
+     transaction2.clean();
+     transaction3.clean();
 
     // Comprobar el restablecimiento
-    transaction.clean();
-    transaction2.clean();
+    transaction2.print("valores en cero, transaccion2:");
+    transaction3.print("valores en cero, transaccion3:");
 
     // Continuar con más pruebas si es necesario
     // ...
@@ -111,6 +127,28 @@ trans_bushandler#(pkg_size, drvrs, broadcast) transaction2;
     // Finalizar la simulación
     $finish;
   end
+  /////////////////FINAL DE LA PRUEBA DE LA CLASE BUS HANDLER///////////
+  
+
+ ///////////////////////////////////////////////////////////////////
+ ///////Creando la Interface que se conecta con la FIFO (DUT)///////
+ ///////////////////////////////////////////////////////////////////
+ 
+interface bushandler_if #( parameter drvrs = 4, parameter pkg_size = 16)//no defino el parametro bits por que es 1 siempre.
+(
+  input clk  
+);
+logic reset;
+logic pndng[0:0][drvrs-1:0];
+logic push[0:0][drvrs-1:0];
+logic pop[0:0][drvrs-1:0];
+logic [pkg_size-1:0] d_pop [0:0][drvrs-1:0];
+logic [pkg_size-1:0] d_push [0:0][drvrs-1:0];
+ 
+endinterface
+
+//////////////////Probando que la interface funcione//////////////////
+
 
 endmodule
 
