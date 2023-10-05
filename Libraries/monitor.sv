c11f6440e1751fdd4a8a4a71de2ed8e2a9618f99
AlexAguero <alexmanuelaguerovargas@gmail.com>
2023-10-05 01:53:17 -0600

    necesito ayuda

--- Libraries/monitor.sv
+++ Libraries/monitor.sv
@@ -1,14 +1,15 @@
-class monitor_hijo #(parameter drvrs=4, parameter pkg_size=16, parameter depth=8 ,parameter broadcast={8{1'b1}});//añadp broadcast=??
+//tengo que hacerl include al driver?
+class monitor_hijo #(parameter drvrs=4, parameter pkg_size=16, parameter depth=8);
 	fifo_sim #(.pkg_size(pkg_size), .depth(depth)) fifo_out;
 	virtual bushandler_if #(.drvrs(drvrs), .pkg_size(pkg_size) ) vif;	
-	trans_bushandler #(.pkg_size(pkg_size), .drvrs(drvrs), .broadcast(broadcast)) transaccion;	 //ver como sale en el interface
+	trans_bushandler #(.pkg_size(pkg_size)) transaccion;	
 	
 	logic [7:0]rx; //destino
 	int tx;//origen
-	bit [pkg_size-1:0]dato;
-	bit [pkg_size-1:0]d_out; //estos estan oK?
+	bit [pkg_size-1:0]dato; //lo que viene del dut
+	bit [pkg_size-1:0]d_out;//lo que va pal checker
 	int retardo;
-	int listo;
+	int listo; //esta wea no va
 	
 	function new;
 		fifo_out = new();
@@ -19,14 +20,15 @@ class monitor_hijo #(parameter drvrs=4, parameter pkg_size=16, parameter depth=8
 	
 	task run();
 	dato = vif.D_push[bits-1][tx];
-	rx = dato[pkg_size-1:pkg_size-8];
-	if (rx == tx && vif.push[bits-1][tx] == 1)begin
+	rx = dato[7:0];
+//    tx= transaccion.dispositivo_tx;// no entiendo el if
+	if (rx == tx && vif.push[0][tx] == 1)begin
 		$display("[%g] Mhijo inicializado",$time);
 		$display("Destino = %g",rx);
 		$display("Dato = %b",dato);
 		$display("--------------------------------------------------------------");
 		$display("[%g] Monitor_hijo: Dispositivo %g listo para recibir",$time,tx);
-		fifo_out.D_push=vif.D_push[bits-1][tx];
+		fifo_out.D_push=vif.D_push[0][tx];
 		fifo_out.push(); 
 		fifo_out.pop();
 		/*transaction.D_pop = fifo_out.D_pop;
@@ -40,9 +42,10 @@ endclass
 
 class monitor_papi #(parameter pkg_size=16, parameter depth=8, parameter drvrs=4, parameter broadcast={8{1'b1}}); 
 	virtual bushandler_if #(.drvrs(drvrs), .pkg_size(pkg_size)) vif;	
-	monitor_hijo #(.drvrs(drvrs),.pkg_size(pkg_size),.depth(depth),.broadcast(broadcast)) mhijo[drvrs-1:0];
-	//trans_bus_mbx mntr_chckr_mbx;// Mailbox del monitor al checker de tipo trans_bus
-	trans_bushandler #(.pkg_size(pkg_size), .drvrs(drvrs), .broadcast(broadcast)) transaccion; 	//ver como lo llame en la interface
+	monitor_hijo #(.drvrs(drvrs),.pkg_size(pkg_size),.depth(depth)) mhijo[drvrs-1:0];
+    trans_bushandler_mbx  monitor_checker_mbx; //mailbox del monitor al checker
+
+    trans_bushandler #(.pkg_size(pkg_size)) transaccion; 
 
 	function new;
     		for (int i=0;i<drvrs; i++)begin
@@ -60,7 +63,8 @@ task run();
 	//@(posedge vif.clk)
 	for (int i=0; i<drvrs; i++)begin
         automatic int newmon=i;
-		mhijo[newmon].vif = vif;
+
+        mhijo[newmon].vif = vif;
 		//mson[k].drv_dson_mbx = agnt_drv_mbx;
 		mhijo[newmon].origen = newmon;
 		mhijo[newmon].run();	
